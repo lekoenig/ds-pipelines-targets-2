2d1e112e6183912f6e301e6f9404c27ae2fa3015
Koenig <lkoenigsnyder@usgs.gov>
2021-10-14 13:24:01 -0400

    refactor pipeline to simplify data processing steps and combine into a single target

--- _targets.R
+++ _targets.R
@@ -43,16 +43,8 @@ p1_targets_list <- list(
 
 p2_targets_list <- list(
   tar_target(
-    site_data_clean, 
-    process_data(site_data)
-  ),
-  tar_target(
-    site_data_annotated,
-    annotate_data(site_data_clean, site_filename = site_info_csv)
-  ),
-  tar_target(
-    site_data_styled,
-    style_data(site_data_annotated)
+    site_data_processed, 
+    process_data(combined_site_data,site_info_csv)
   )
 )
 
