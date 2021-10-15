39d53dbfa9de83df7dba00e2f4fc6671f42b0e9e
Koenig <lkoenigsnyder@usgs.gov>
2021-10-15 12:56:15 -0400

    add process phase targets to 1_process.R and edit target names accordingly

---
+++ 2_process.R
@@ -0,0 +1,10 @@
+source("2_process/src/process_and_style.R")
+tar_option_set(packages = c("tidyverse")) 
+
+p2_targets_list <- list(
+  tar_target(
+    p2_site_data_processed_csv, 
+    process_data(fileout = "2_process/out/site_data_processed.csv",p1_combined_site_data,p1_site_info),
+    format = "file"
+  )
+)
